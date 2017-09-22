e5b59b66c277ad784a9d3f9fce1f99698c6a81b6
Adam Plumer <caerus.karu@gmail.com>
2017-09-22 19:39:50 -0400

    Changing ci scripts to copy subdirectories instead of top level

--- scripts/ci/build-prod.sh
+++ scripts/ci/build-prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rp --delete --quiet $TRAVIS_BUILD_DIR $SSH_USER@$HOST:$PROD_PATH/backend/. || exit 29
+rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/* $SSH_USER@$HOST:$PROD_PATH/backend/. || exit 29
