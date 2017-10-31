a0a5583026da0b62c0e60326ea7d27f0690cc0e0
Adam Plumer <caerus.karu@gmail.com>
2017-10-31 05:02:22 -0400

    build: make travis pass node_modules

--- scripts/ci/build-dev.sh
+++ scripts/ci/build-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/node_modules $SSH_USER@$HOST:$DEV_PATH/backend/. || exit 29
 rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/dist/* $SSH_USER@$HOST:$DEV_PATH/backend/. || exit 29
