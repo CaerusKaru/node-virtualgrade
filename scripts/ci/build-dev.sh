a5daa8b4384dda3b625e4f1fa6b7e58c1d25c69b
Adam Plumer <caerus.karu@gmail.com>
2017-12-01 22:18:41 -0500

    fix: update tsconfig to ignore migrations

--- scripts/ci/build-dev.sh
+++ scripts/ci/build-dev.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/node_modules $SSH_USER@$HOST:$DEV_PATH/backend/. || exit 29
+ssh $SSH_USER@$HOST mkdir -p $DEV_PATH/backend/db || exit 29
+rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/migrations $SSH_USER@$HOST:$DEV_PATH/backend/db/. || exit 29
+rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/seeds $SSH_USER@$HOST:$DEV_PATH/backend/db/. || exit 29
+rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/knexfile.ts $SSH_USER@$HOST:$DEV_PATH/backend/db/. || exit 29
 rsync -rp --delete --quiet $TRAVIS_BUILD_DIR/dist/* $SSH_USER@$HOST:$DEV_PATH/backend/. || exit 29
